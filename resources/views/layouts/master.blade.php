c3cf2683bb1f0f639ab9f9b9b6b66b25cb12632c
kalzen <manhto109@gmail.com>
2025-03-22 01:24:52 +0700

    push floating social

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,13 +5,14 @@
     <meta charset="utf-8">
     <title>Bất động sản Phía Tây</title>
 
-    <meta name="author" content="themesflat.com">
+    <meta name="author" content="12bytes.xyz">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
     <link rel="stylesheet" href="{{asset('phiatay/app/dist/font-awesome.css')}}">
     <link rel="stylesheet" href="{{asset('phiatay/app/dist/app.css')}}">
     <link rel="stylesheet" href="{{asset('phiatay/app/dist/responsive.css')}}">
     <link rel="stylesheet" href="{{asset('phiatay/app/dist/owl.css')}}">
+    <link rel="stylesheet" href="{{asset('phiatay/app/dist/floating-social.css')}}">
 
     <!-- Favicon and Touch Icons  -->
     <link rel="shortcut icon" href="{{asset('favicon.png')}}">
@@ -47,14 +48,41 @@
         @include('partials.header')
         @yield('content')
         @include('partials.footer')
+        
+        <!-- Floating Social Icons -->
+        <div class="floating-social">
+            <div class="floating-social-menu">
+                <a href="tel:+840858050050" class="floating-social-item phone-item">
+                    <i class="fa fa-phone"></i>
+                </a>
+                <a href="https://zalo.me/0858050050" target="_blank" class="floating-social-item zalo-item">
+                    <i class="fa fa-comment"></i>
+                </a>
+                <a href="https://m.me/101266363070532" target="_blank" class="floating-social-item facebook-item">
+                    <i class="fab fa-facebook-messenger"></i>
+                </a>
+            </div>
+            <div class="floating-social-toggle">
+                <i class="fa fa-plus"></i>
+            </div>
+        </div>
+        
         @section('scripts')
         @show
-                  
-
-
-           
+        
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                const toggle = document.querySelector('.floating-social-toggle');
+                const menu = document.querySelector('.floating-social-menu');
+                
+                toggle.addEventListener('click', function() {
+                    menu.classList.toggle('active');
+                    toggle.classList.toggle('active');
+                });
+            });
+        </script>
+    </div>
+</div>           
 </body>
-
-
 </html>
 
